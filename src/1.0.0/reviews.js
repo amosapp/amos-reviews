e54c52965eeb6214ecc954ed0b336b1c91f0e02a
dteiml <dteiml@gmail.com>
2019-09-24 13:14:54 +0200

    add one review

---
+++ src/1.0.0/reviews.js
@@ -0,0 +1,11 @@
+/**
+ * @description Format is [type, links, topics, prerequisites]
+ */
+module.exports = [
+  [
+    [`lecture-notes`],
+    [`https://www.cl.cam.ac.uk/teaching/1819/Algorithms/`],
+    [`DataStructuresAndAlgorithms`],
+    []
+  ]
+]
