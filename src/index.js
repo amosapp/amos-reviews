3e0d80ddfae49fd4df5b1173890963984732efc0
dteiml <dteiml@gmail.com>
2019-10-06 13:22:41 +0200

    add goodreads

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const hackprague = require (`./1.0.0/hackprague2019`)
+const goodreads = require (`./1.0.0/goodreads`)
+const others = require (`./1.0.0/wiki+blinkist+openlibrary`)
+
+module.exports = {
+  hackprague,
+  goodreads,
+  others
+}
